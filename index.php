a4023a17eda562b2f0c1b3fc66a4e418f7a82cdb
Dolgov Dmitriy <TwilightTower@mail.ru>
2017-07-10 20:16:36 +0300

    short description finished

--- index.php
+++ index.php
@@ -3,40 +3,46 @@
 error_reporting(E_ALL ^ E_WARNING);
 ini_set('display_errors', 1);
 
-$url = 'https://iclim.ru/catalog/ventilyatsiya/ventilyatsionnye_ustanovki/pritochno_vytyazhnye_ustanovki/tag/ostberg/?filter=arCatalogFilter_20_195255756:Y&sort=shows';
+$urlRoot = 'https://iclim.ru';
+$url = $urlRoot . '/catalog/ventilyatsiya/ventilyatsionnye_ustanovki/pritochno_vytyazhnye_ustanovki/tag/ostberg/?filter=arCatalogFilter_20_195255756:Y&sort=shows';
 
 $dom = new DOMDocument;
 $dom->preserveWhiteSpace = false;
-
 $load = $dom->loadHTMLFile($url);
-
 $xpath = new DOMXPath($dom);
 
-//id=catalog_list
-
-//$catalog = $dom->getElementById("catalog_list");
-
-#$attr = $xpath->query("//*[@id='catalog_list']")->item(0)->getAttribute('style');
-
-#$attr = $xpath->query("//bx_catalog_item_container/a")->item(0)->getAttribute('style');
-
-$items = $xpath->query("//div[@id='catalog_list']/div[@class='bx_catalog_item']");
+$items = $xpath->query("//div[@id='catalog_list']/div[@class='bx_catalog_item']/div[@class='bx_catalog_item_container']");
 
 $products = [];
 
 foreach ($items as $itm) {
     $prod = [];
 
-    $imgElement = $xpath->query("./div/a[@class='bx_catalog_item_images']", $itm)->item(0);
+    $imgElem = $xpath->query("./a[@class='bx_catalog_item_images']", $itm)->item(0);
 
-    $imgStyle = $imgElement->getAttribute('style');
-    preg_match("url\s*\(\s*['\"]\s*(.*)\s*['\"]\s*\)/U", $imgStyle, $matches);
+    $imgStyle = $imgElem->getAttribute('style');
+    preg_match("/url\s*\(\s*['\"]\s*(.*)\s*['\"]\s*\)/U", $imgStyle, $matches);
     $prod['img_src'] = $matches[1];
 
-    $prod['href_full_description'] = $imgElement->getAttribute('href');
+    $prod['href_full_description'] = $imgElem->getAttribute('href');
 
-    $products = $prod;
+    $prod['name'] = $xpath->query("./div[@class='discribe']/div[@class='bx_catalog_item_title']/a", $itm)->item(0)->nodeValue;
+    $prod['short_description'] = $xpath->query("./div[@class='discribe']/div[@class='prev_txt']", $itm)->item(0)->nodeValue;
+
+    // Переход к подробностям
+    $fullUrl = $urlRoot . $prod['href_full_description'];
+
+    $domFull = new DOMDocument;
+    $domFull->preserveWhiteSpace = false;
+    $loadFull = $domFull->loadHTMLFile($fullUrl);
+    $xpathFull = new DOMXPath($domFull);
+
+    $descrFullElem = $xpath->query("//div[@id='desc_txt_tab']/div/div[@class='bx_item_description']")->item(0)->nodeValue;
+
+
+
+    $products = array_map('trim', $prod);
 }
 
-echo $attr;
+echo 'fin';
 
