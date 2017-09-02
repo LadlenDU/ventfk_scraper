620e058a6e6c40d3524b5a0a83d06512dc41a601
Ladlen <TwilightTower@mail.ru>
2017-09-03 02:57:02 +0300

    separate parse brand function

--- functions.php
+++ functions.php
@@ -48,3 +48,173 @@ function putProduct($prod, $urlRoot, $cid, $priceCorrectPercent, &$newItems, &$o
 
     return true;
 }
+
+function parseBrand($url, $cid, $percent, $cat_name, $email)
+{
+    //$url = 'https://iclim.ru/catalog/ventilyatsiya/ventilyatsionnye_ustanovki/pritochno_vytyazhnye_ustanovki/tag/ostberg/?filter=arCatalogFilter_20_195255756:Y&sort=shows';
+    $origUrl = $url = trim($url);
+
+    $urlElements = parse_url($url);
+    $urlRoot = $urlElements['scheme'] . '://' . $urlElements['host'];
+
+    //$cid = 'cid_4951221';   // Ostberg
+    $cid = trim($cid);
+    $pricePercent = trim($percent);
+
+    $dom = new DOMDocument;
+    $dom->preserveWhiteSpace = false;
+
+    $products = array();
+
+    $oldItems = array();
+    $newItems = array();
+    $wrongItems = array();
+
+//    header('Content-Type: text/html; charset=utf-8');
+
+    $stubCounter = 1;
+
+    do {
+
+        if ($stubCounter++ > PAGES_LIMIT) {
+            break;
+        }
+
+        $load = $dom->loadHTMLFile($url);
+        $xpath = new DOMXPath($dom);
+
+        $items = $xpath->query("//div[@id='catalog_list']/div[@class='bx_catalog_item']/div[@class='bx_catalog_item_container']");
+
+        foreach ($items as $itmKey => $itm) {
+            $prod = array();
+
+            $imgElem = $xpath->query("./a[@class='bx_catalog_item_images']", $itm)->item(0);
+            if (!$imgElem) {
+                $wrongItems[] = array('url' => $url, 'key' => $itmKey, 'stage' => 1);
+                continue;
+            }
+
+            $imgStyle = $imgElem->getAttribute('style');
+            preg_match("/url\s*\(\s*['\"]\s*(.*)\s*['\"]\s*\)/U", $imgStyle, $matches);
+            $prod['img_src'] = $matches[1];
+
+            $prod['href_full_description'] = $imgElem->getAttribute('href');
+
+            $tmp = $xpath->query("./div[@class='discribe']/div[@class='bx_catalog_item_title']/a", $itm)->item(0);
+            if (!$tmp) {
+                $tmp[] = array('url' => $url, 'key' => $itmKey, 'stage' => 2);
+                continue;
+            }
+            $prod['name'] = $tmp->nodeValue;
+            $tmp = $xpath->query("./div[@class='discribe']/div[@class='prev_txt']", $itm)->item(0);
+            if (!$tmp) {
+                $tmp[] = array('url' => $url, 'key' => $itmKey, 'stage' => 3);
+                continue;
+            }
+            $prod['short_description'] = $tmp->nodeValue;
+
+            // Переход к подробностям
+            $fullUrl = $urlRoot . $prod['href_full_description'];
+
+            $domFull = new DOMDocument;
+            $domFull->preserveWhiteSpace = false;
+            $fContent = file_get_contents($fullUrl);
+            $fContent = str_replace('<!--<div class="item_b"><span class="icon icon_delivery"></span>Бесплатная&nbsp;доставка&nbsp;до&nbsp;транспортной&nbsp;компании</div>--!>	', '', $fContent);
+            $loadFull = $domFull->loadHTML($fContent);
+            //$loadFull = $domFull->loadHTMLFile($fullUrl);
+            $xpathFull = new DOMXPath($domFull);
+
+            $descrFullElem = $xpathFull->query("//div[@id='desc_txt_tab']/div/div[@class='bx_item_description']")->item(0);
+            $featureFullElem = $xpathFull->query("//div[@id='prop_txt_tab']/div/div[@class='item_info_section']")->item(0);
+            $priceFullElem = $xpathFull->query("//div[@class='item_current_price']")->item(0);
+            $vendorCodeFullElem = $xpathFull->query("//div[@class='detail_articul']/span")->item(0);
+
+            if (!$descrFullElem && !$featureFullElem) {
+                $wrongItems[] = array('url' => $fullUrl, 'key' => $itmKey, 'stage' => 4);
+                continue;
+            }
+            /*if (!$priceFullElem) {
+                $wrongItems[] = array('url' => $fullUrl, 'key' => $itmKey, 'stage' => 5);
+                continue;
+            }*/
+
+            $prod['full_description'] = $descrFullElem ? $descrFullElem->ownerDocument->saveHTML($descrFullElem) : '';
+            $prod['full_feature'] = $featureFullElem ? $featureFullElem->ownerDocument->saveHTML($featureFullElem) : '';
+            $prod['price'] = $priceFullElem ? str_replace(array('руб.', 'руб', ' '), '', $priceFullElem->textContent) : 0;
+            $prod['vendor_code'] = trim($vendorCodeFullElem->textContent);
+
+            // Характеристики
+            $domFeature = new DOMDocument;
+            $domFeature->preserveWhiteSpace = false;
+            $loadFeature = $domFeature->loadHTML('<?xml encoding="utf-8" ?>' . $prod['full_feature']);
+            $xpathFeature = new DOMXPath($domFeature);
+
+            $prod['features'] = array();
+
+            $trs = $xpathFeature->query("//div[@class='item_info_section']/table/tr");
+            foreach ($trs as $tr) {
+
+                $feature = array();
+
+                $counter = 0;
+                foreach ($tr->childNodes as $node) {
+                    if ($node->nodeName == 'td') {
+                        $textContent = DataReader::normalizeString($node->textContent);
+                        if ($counter == 0) {
+                            ++$counter;
+                            if (mb_strtolower($textContent, 'utf-8') == 'бренд') {
+                                break;
+                            }
+                            $feature['name'] = $textContent;
+                        } else {
+                            $feature['value'] = $textContent;
+                            break;
+                        }
+                    }
+                }
+
+                if ($feature) {
+                    $prod['features'][] = $feature;
+                }
+            }
+
+            $products[] = array_map_recursive('trim', $prod);
+            sleep(SLEEP_BEFORE_PUT_PRODUCT);
+            putProduct($prod, $urlRoot, $cid, $pricePercent, $newItems, $oldItems, $wrongItems);
+        }
+
+        $notLastPage = false;
+
+        //.catalog_pageNav .navstring ul li.bx-pag-next a (get href)
+        //$nextPage = $xpath->query("//div[@id='catalog_list']/div[@class='bx_catalog_item']/div[@class='bx_catalog_item_container']");
+        $nextPage = $xpath->query("//div[@class='catalog_pageNav']/div[@class='navstring']/ul/li[@class='bx-pag-next']/a");
+        if ($nextPage->length) {
+            if ($newUrl = $nextPage->item(0)->getAttribute('href')) {
+                $url = $urlRoot . $newUrl;
+                $notLastPage = true;
+            }
+        }
+
+    } while ($notLastPage);
+
+    $cat_name = trim($cat_name);
+    $email = trim($email);
+
+    $str = "Завершен парсинг страницы\n"
+        . "$origUrl\nв пункт '$cat_name, id:$cid'\n"
+        . "Отчет отправлен на адрес $email\n"
+        . "Добавлены элементы:\n"
+        . print_r($newItems, true) . "\n\n"
+        . "Элементы, проигнорированные как уже существующие:\n"
+        . print_r($oldItems, true) . "\n\n"
+        . "Пропущенные по причине ошибок элементы:\n"
+        . print_r($wrongItems, true);
+
+    $resultString = "<pre>\n$str\n</pre>";
+
+    $msg = date(DATE_RFC822) . " >\n$str\n\n";
+    error_log($msg, 3, RESULT_LOG_FILE);
+
+    mail($email, "Произведен парсинг для категории $cat_name", $msg);
+    //exit;
+}
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Content-Type: text/html; charset=utf-8');
+
 require_once('configCommon.php');
 require_once('config.php');
 
@@ -10,172 +12,21 @@ require_once('functions.php');
 
 if (!empty($_POST['cid'])) {
 
-    //$url = 'https://iclim.ru/catalog/ventilyatsiya/ventilyatsionnye_ustanovki/pritochno_vytyazhnye_ustanovki/tag/ostberg/?filter=arCatalogFilter_20_195255756:Y&sort=shows';
-    $origUrl = $url = trim($_POST['url']);
-
-    $urlElements = parse_url($url);
-    $urlRoot = $urlElements['scheme'] . '://' . $urlElements['host'];
-
-    //$cid = 'cid_4951221';   // Ostberg
-    $cid = trim($_POST['cid']);
-    $pricePercent = trim($_POST['percent']);
-
-    $dom = new DOMDocument;
-    $dom->preserveWhiteSpace = false;
-
-    $products = array();
-
-    $oldItems = array();
-    $newItems = array();
-    $wrongItems = array();
-
-    header('Content-Type: text/html; charset=utf-8');
-
-    $stubCounter = 1;
-
-    do {
-
-        if ($stubCounter++ > PAGES_LIMIT) {
-            break;
-        }
-
+    if (!empty($_POST['parse_brands'])) {
+        $url = trim($_POST['url']);
+        $dom = new DOMDocument;
+        $dom->preserveWhiteSpace = false;
         $load = $dom->loadHTMLFile($url);
         $xpath = new DOMXPath($dom);
-
-        $items = $xpath->query("//div[@id='catalog_list']/div[@class='bx_catalog_item']/div[@class='bx_catalog_item_container']");
-
-        foreach ($items as $itmKey => $itm) {
-            $prod = array();
-
-            $imgElem = $xpath->query("./a[@class='bx_catalog_item_images']", $itm)->item(0);
-            if (!$imgElem) {
-                $wrongItems[] = array('url' => $url, 'key' => $itmKey, 'stage' => 1);
-                continue;
-            }
-
-            $imgStyle = $imgElem->getAttribute('style');
-            preg_match("/url\s*\(\s*['\"]\s*(.*)\s*['\"]\s*\)/U", $imgStyle, $matches);
-            $prod['img_src'] = $matches[1];
-
-            $prod['href_full_description'] = $imgElem->getAttribute('href');
-
-            $tmp = $xpath->query("./div[@class='discribe']/div[@class='bx_catalog_item_title']/a", $itm)->item(0);
-            if (!$tmp) {
-                $tmp[] = array('url' => $url, 'key' => $itmKey, 'stage' => 2);
-                continue;
-            }
-            $prod['name'] = $tmp->nodeValue;
-            $tmp = $xpath->query("./div[@class='discribe']/div[@class='prev_txt']", $itm)->item(0);
-            if (!$tmp) {
-                $tmp[] = array('url' => $url, 'key' => $itmKey, 'stage' => 3);
-                continue;
-            }
-            $prod['short_description'] = $tmp->nodeValue;
-
-            // Переход к подробностям
-            $fullUrl = $urlRoot . $prod['href_full_description'];
-
-            $domFull = new DOMDocument;
-            $domFull->preserveWhiteSpace = false;
-            $fContent = file_get_contents($fullUrl);
-            $fContent = str_replace('<!--<div class="item_b"><span class="icon icon_delivery"></span>Бесплатная&nbsp;доставка&nbsp;до&nbsp;транспортной&nbsp;компании</div>--!>	', '', $fContent);
-            $loadFull = $domFull->loadHTML($fContent);
-            //$loadFull = $domFull->loadHTMLFile($fullUrl);
-            $xpathFull = new DOMXPath($domFull);
-
-            $descrFullElem = $xpathFull->query("//div[@id='desc_txt_tab']/div/div[@class='bx_item_description']")->item(0);
-            $featureFullElem = $xpathFull->query("//div[@id='prop_txt_tab']/div/div[@class='item_info_section']")->item(0);
-            $priceFullElem = $xpathFull->query("//div[@class='item_current_price']")->item(0);
-            $vendorCodeFullElem = $xpathFull->query("//div[@class='detail_articul']/span")->item(0);
-
-            if (!$descrFullElem && !$featureFullElem) {
-                $wrongItems[] = array('url' => $fullUrl, 'key' => $itmKey, 'stage' => 4);
-                continue;
-            }
-            /*if (!$priceFullElem) {
-                $wrongItems[] = array('url' => $fullUrl, 'key' => $itmKey, 'stage' => 5);
-                continue;
-            }*/
-
-            $prod['full_description'] = $descrFullElem ? $descrFullElem->ownerDocument->saveHTML($descrFullElem) : '';
-            $prod['full_feature'] = $featureFullElem ? $featureFullElem->ownerDocument->saveHTML($featureFullElem) : '';
-            $prod['price'] = $priceFullElem ? str_replace(array('руб.', 'руб', ' '), '', $priceFullElem->textContent) : 0;
-            $prod['vendor_code'] = trim($vendorCodeFullElem->textContent);
-
-            // Характеристики
-            $domFeature = new DOMDocument;
-            $domFeature->preserveWhiteSpace = false;
-            $loadFeature = $domFeature->loadHTML('<?xml encoding="utf-8" ?>' . $prod['full_feature']);
-            $xpathFeature = new DOMXPath($domFeature);
-
-            $prod['features'] = array();
-
-            $trs = $xpathFeature->query("//div[@class='item_info_section']/table/tr");
-            foreach ($trs as $tr) {
-
-                $feature = array();
-
-                $counter = 0;
-                foreach ($tr->childNodes as $node) {
-                    if ($node->nodeName == 'td') {
-                        $textContent = DataReader::normalizeString($node->textContent);
-                        if ($counter == 0) {
-                            ++$counter;
-                            if (mb_strtolower($textContent, 'utf-8') == 'бренд') {
-                                break;
-                            }
-                            $feature['name'] = $textContent;
-                        } else {
-                            $feature['value'] = $textContent;
-                            break;
-                        }
-                    }
-                }
-
-                if ($feature) {
-                    $prod['features'][] = $feature;
-                }
-            }
-
-            $products[] = array_map_recursive('trim', $prod);
-            sleep(SLEEP_BEFORE_PUT_PRODUCT);
-            putProduct($prod, $urlRoot, $cid, $pricePercent, $newItems, $oldItems, $wrongItems);
-        }
-
-        $notLastPage = false;
-
-        //.catalog_pageNav .navstring ul li.bx-pag-next a (get href)
-        //$nextPage = $xpath->query("//div[@id='catalog_list']/div[@class='bx_catalog_item']/div[@class='bx_catalog_item_container']");
-        $nextPage = $xpath->query("//div[@class='catalog_pageNav']/div[@class='navstring']/ul/li[@class='bx-pag-next']/a");
-        if ($nextPage->length) {
-            if ($newUrl = $nextPage->item(0)->getAttribute('href')) {
-                $url = $urlRoot . $newUrl;
-                $notLastPage = true;
-            }
-        }
-
-    } while ($notLastPage);
-
-    $cat_name = trim($_POST['cat_name']);
-    $email = trim($_POST['email']);
-
-    $str = "Завершен парсинг страницы\n"
-        . "$origUrl\nв пункт '$cat_name, id:$cid'\n"
-        . "Отчет отправлен на адрес $email\n"
-        . "Добавлены элементы:\n"
-        . print_r($newItems, true) . "\n\n"
-        . "Элементы, проигнорированные как уже существующие:\n"
-        . print_r($oldItems, true) . "\n\n"
-        . "Пропущенные по причине ошибок элементы:\n"
-        . print_r($wrongItems, true);
-
-    $resultString = "<pre>\n$str\n</pre>";
-
-    $msg = date(DATE_RFC822) . " >\n$str\n\n";
-    error_log($msg, 3, RESULT_LOG_FILE);
-
-    mail($email, "Произведен парсинг для категории $cat_name", $msg);
-    //exit;
+        //div.bx-filter-parameters-box-title span.bx-filter-parameters-box-hint
+        //Бренд >
+        //div.bx-filter-block
+        //bx-filter-input-checkbox
+        //div.bx-filter-parameters-box:nth-child(3) > div:nth-child(3) > div:nth-child(1)
+        //html.bx-core.bx-no-touch.bx-no-retina.bx-firefox body div#main div#content div div div.container.catalog_list_page div.row div.col-lg-3.col-md-3.col-sm-14.col-xs-14.catalog_left_collumn div.bx-sidebar-block div.slide_block div.slide_block_content div.bx-filter div.bx-filter-section form.smartfilter div.bx-filter-parameters-box.bx-active div.bx-filter-block div.bx-filter-parameters-box-container
+    } else {
+        parseBrand($_POST['url'], $_POST['cid'], $_POST['percent'], $_POST['cat_name'], $_POST['email']);
+    }
 }
 
 $dr = new DataReader();
